f10da8c328276514ab9bb71ad9abe0411a566586
Javier Sáez García <javiersaez1205@gmail.com>
2022-10-17 19:22:12 +0200

    miniCalculadoraApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'miniCalculadoraApp';
+  title = 'Mini Calculadora';
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
+  resultado2: string ="";
+
+  sumar(event:Event):void{
+      this.resultado = this.numero1 + this.numero2
+  }
+  restar(event:Event):void{
+      this.resultado = this.numero1 - this.numero2
+  }
+  multiplicar(event:Event):void{
+    this.resultado = this.numero1 * this.numero2
+  }
+  dividir(event:Event):void{
+    if(this.numero2>0){
+      this.resultado = this.numero1 / this.numero2;
+    }else{
+      this.resultado2 = "No se puede dividir por cero o menor de cero";
+      
+    }
+    
+  }
+
 }
